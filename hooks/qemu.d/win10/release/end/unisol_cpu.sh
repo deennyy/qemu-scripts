b1954c2e7645d19f31f1d28d7e38f5273bd60dbe
Denislav Rashkov <70910384+deennyy@users.noreply.github.com>
2023-03-14 23:54:38 +0200

    Delete unisol_cpu.sh

--- hooks/qemu.d/win10/release/end/unisol_cpu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-systemctl set-property --runtime -- user.slice AllowedCPUs=0-11
-systemctl set-property --runtime -- system.slice AllowedCPUs=0-11
-systemctl set-property --runtime -- init.scope AllowedCPUs=0-11
